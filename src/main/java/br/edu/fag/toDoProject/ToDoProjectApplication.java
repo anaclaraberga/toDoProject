60b1667f0ed13cb067776602c5b035ca748495af
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-02-23 21:28:20 -0300

    Inicializando

---
+++ src/main/java/br/edu/fag/toDoProject/ToDoProjectApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fag.toDoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoProjectApplication.class, args);
+	}
+
+}
