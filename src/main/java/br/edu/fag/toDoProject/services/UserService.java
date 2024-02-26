976af8ac299030acc68883c533785f538f492d91
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-02-26 20:22:22 -0300

    Criação do CRUD

--- src/main/java/br/edu/fag/toDoProject/services/UserService.java
+++ src/main/java/br/edu/fag/toDoProject/services/UserService.java
@@ -1,16 +1,25 @@
 package br.edu.fag.toDoProject.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.edu.fag.toDoProject.models.User;
 import br.edu.fag.toDoProject.repositories.TaskRepository;
 import br.edu.fag.toDoProject.repositories.UserRepository;
 
 @Service
 public class UserService {
-    
+
+    @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private TaskRepository taskRepository;
 
-
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow();
+    }
 }
