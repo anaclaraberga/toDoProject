e252b8af3f0525b861100eac665ebc11fb7e6d7d
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-02-28 19:17:06 -0300

    Criação do User Service

---
+++ src/main/java/br/edu/fag/toDoProject/services/TaskService.java
@@ -0,0 +1,11 @@
+package br.edu.fag.toDoProject.services;
+
+import org.springframework.stereotype.Service;
+
+import br.edu.fag.toDoProject.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    private TaskRepository taskRepository;
+    
+}
--- src/main/java/br/edu/fag/toDoProject/services/UserService.java
+++ src/main/java/br/edu/fag/toDoProject/services/UserService.java
@@ -2,8 +2,11 @@ package br.edu.fag.toDoProject.services;
 
 import java.util.Optional;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.fag.toDoProject.models.User;
 import br.edu.fag.toDoProject.repositories.TaskRepository;
@@ -20,6 +23,31 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
-        return user.orElseThrow();
+        return user.orElseThrow(() -> new RuntimeException(
+            "Usuário não encontrado! Id: " + id + ", Tipos: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas.");
+        }
     }
 }
