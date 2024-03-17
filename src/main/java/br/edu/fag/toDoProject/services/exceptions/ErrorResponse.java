8c81ef9e8bcd6831110088ae5f16ec7528b408a5
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-03-17 19:55:45 -0300

    Tratamento de Exceptions

--- src/main/java/br/edu/fag/toDoProject/services/exceptions/ErrorResponse.java
+++ src/main/java/br/edu/fag/toDoProject/services/exceptions/ErrorResponse.java
@@ -1,5 +1,36 @@
 package br.edu.fag.toDoProject.services.exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
-    
+    private final int status;
+    private final String message;
+    private String stackTrace;
+    private List<ValidationError> errors;
+
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    private static class ValidationError {
+        private final String field;
+        private final String message;
+    }
+
+    public void addValidationError(String field, String message) {
+        if (Objects.isNull(errors)) {
+            this.errors = new ArrayList<>();
+        }
+        this.errors.add(new ValidationError(field, message));
+    }
 }
